95163ed28b489a2a30529574d0c42f1b474547ac
Michel França <fr.mich17@gmail.com>
2021-04-13 21:12:58 -0300

    Novo design sendo implementado em partes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="app" class="flex flex-col items-center justify-center h-screen gap-5 px-3">
-    <div class="relative w-full h-auto p-5 rounded-md md:w-2/4 app-container">
-      <header class="user-container">
+    <div class="w-full h-auto p-5 rounded-md md:w-2/4 app-container">
+      <header class="flex justify-center mb-5 user-container">
+        <button 
+          title="Adicionar um novo item"
+          class="flex items-center gap-3 p-4 text-base font-medium text-white duration-200 bg-green-600 rounded-full shadow-md hover:bg-green-700"
+          @click="showAdd"
+        >
+          <img src="@/assets/add.svg" alt="Adicionar novo Todo" class="w-5">
+          Adicionar um novo item a lista de ToDo
+        </button>
+      </header>
+      <main class="px-10 todo-container">
         <div
           v-if="showNewTodo"
           class="fixed top-0 left-0 z-10 flex flex-col items-center justify-center w-screen h-screen gap-3 px-64"
           >
           <input
             type="text"
-            class="w-full p-5 font-medium text-gray-300 rounded-md shadow-sm outline-none"
-            placeholder="Escreve aqui seu novo item na lista ..."
+            class="w-full p-5 text-lg font-medium text-center text-gray-300 rounded-md shadow-sm outline-none"
+            placeholder="Escreva aqui a descrição do seu novo item..."
             v-model="todo.description"
           >
           <button 
-            class="w-1/4 p-2 font-medium text-white rounded-md"
+            class="w-1/4 p-3 font-medium text-white duration-200 bg-green-600 rounded-md hover:bg-green-700"
             @click="addTodo(todo)">Adicionar</button>
         </div>
-        <button 
-          title="Adicionar um novo item"
-          class="absolute flex items-center gap-3 p-4 ml-5 text-base font-medium text-white duration-200 bg-green-600 rounded-full bottom-1/2 md:left-full drop-shadow-sm hover:bg-green-700"
-          @click="showAdd"
-        >
-          <img src="@/assets/add.svg" alt="Adicionar novo Todo" class="w-5">
-        </button>
-      </header>
-      <main class="px-10 todo-container">
         <Todo v-for="t in todos" :key="t.id" :todo="t" @toggle="toggleTodo" @remove="removeTodo" />
       </main>
     </div>
@@ -85,29 +86,15 @@ export default {
   /* background: linear-gradient(-30deg, rgba(240,98,146,1) 0%, rgba(229,92,138,1) 50%, rgba(229,92,138,1) 100%); */
 }
 
-.app-container {
-  background: #3C424A;
-}
-
-header input {
+main input {
   background: #343A40;
 }
 
-header button {
+main button, header button {
   outline: none;
 }
 
-header div {
+main div {
   background: rgba(0, 0, 0, 0.5);
-}
-
-header div button {
-  background: #373C44;
-  transition: .2s;
-  outline: none;
-}
-
-header div button:hover {
-  background: #f06292;
 }
 </style>
